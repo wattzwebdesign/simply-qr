258cbdb3ab2672b68c5632ca058cf0cf97cde6e0
donwattz1459 <mcguinn.donald@gmail.com>
2025-11-09 10:12:23 -0500

    f

--- functions/_middleware.ts
+++
@@ -1,9 +0,0 @@
-// Cloudflare Pages Functions middleware
-// This makes D1 and R2 bindings available to Next.js API routes
-
-export async function onRequest(context: any) {
-  // Attach Cloudflare bindings to the request
-  context.request.env = context.env
-
-  return await context.next()
-}
